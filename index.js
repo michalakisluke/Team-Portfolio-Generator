91dee09567c87fb7e2f81bdc86bceb456559e94a
Luke Michalakis <michalakisluke@gmail.com>
2021-10-13 19:38:17 -0400

    <Build out inquirer functionality>

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const Engineer = require('./lib/Engineer');
         // take manager object and employee array, generate html based on those objects
 
 function init() {
-    // Prompt for team manager infor
+    // Prompt for team manager info
     inquirer.prompt([
         {
             message: "Please enter the project manager's name",
@@ -40,11 +40,11 @@ function init() {
             name: "office",
             type: "input"
         }
-    ])
-    // .then(response => response.selection);
-    const manager = new Manager(answers.name, answers.id, answers.email, answers.office)
-    console.log(manager);
-    //const team = createTeam()
+    ]).then((answers) => {
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.office);
+        console.log(manager);
+        createTeam(); 
+    });
 }
 
 function createTeam() {
@@ -56,11 +56,13 @@ function createTeam() {
     switch(userSelection) {
         case "Add an engineer":
             team.push(engineerPrompt());
+            createTeam();
         case "Add an intern":
             team.push(internPrompt());
+            createTeam();
         case "Complete my team":
+            console.log(team);
     }
-
     return team;
 }
 
@@ -68,11 +70,71 @@ function displayMenu() {
     inquirer.prompt([
         {
             message: "Please select a menu item",
+            name: "menu",
             type: "list",
             choices: ["Add an engineer", "Add an intern", "Complete my team"]
         }
     ])
-    .then(response => response.selection)
+    .then(response => {
+        console.log(response);
+    });
+}
+
+function engineerPrompt() {
+    inquirer.prompt([
+        {
+            message: "Please enter the engineer's name",
+            name: "name",
+            type: "input"
+        },
+        {
+            message: "Please enter the engineer's ID number",
+            name: "id",
+            type: "input"
+        },
+        {
+            message: "Please enter the engineer's email",
+            name: "email",
+            type: "input"
+        },
+        {
+            message: "Please enter the engineer's github username",
+            name: "github",
+            type: "input"
+        }
+    ]).then((answers) => {
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        console.log(engineer);
+    });
+}
+
+function internPrompt() {
+    inquirer.prompt([
+        {
+            message: "Please enter the intern's name",
+            name: "name",
+            type: "input"
+        },
+        {
+            message: "Please enter the intern's ID number",
+            name: "id",
+            type: "input"
+        },
+        {
+            message: "Please enter the intern's email",
+            name: "email",
+            type: "input"
+        },
+        {
+            message: "Please enter the intern's school",
+            name: "school",
+            type: "input"
+        }
+    ])
+    .then((answers) => {
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+        console.log(intern);
+    });
 }
 
 init();
