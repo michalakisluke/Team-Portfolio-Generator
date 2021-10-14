a2166cfe6514779320d87cb6a3d0bb8e2fec83c8
Luke Michalakis <michalakisluke@gmail.com>
2021-10-14 15:53:29 -0400

    <Build template literal for HTML, include employeeHTML function call in template literal>

--- index.js
+++ index.js
@@ -7,18 +7,6 @@ const engineerHTML = require('./lib/engineerGenHTML');
 const internHTML = require('./lib/internGenHTML');
 const team = [];
 
-// start the application
-    // inquirer prompt for team manager information
-        // create a manager using input from the user
-// display menu
-    // user selects intern or engineer
-        // user gets prompted with questions for intern or engineer
-            // createIntern method / createEngineer method
-        // Create inter or engineer object using the inputs
-        // Push this new object to an output array
-    // User selects complete my team
-        // take manager object and employee array, generate html based on those objects
-
 function init() {
     // Prompt for team manager info
     inquirer.prompt([
@@ -50,7 +38,6 @@ function init() {
 }
 
 function createTeam(response) {
-    // Display the menu with "new engineer" "new intern" "complete my team" options
     switch(response.menu) {
         case "Add an engineer":
             engineerPrompt();
@@ -139,17 +126,46 @@ function internPrompt() {
 }
 
 function generateHTML() {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="../../src/style.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <title>Team Portfolio</title>
+    </head>
+    <body>
+        <header>
+            <h1 id = page-title>My Team</h1>
+        </header>
+        <div class = container>
+            <div class = row>
+                <div class = "team-members col-12">
+                    ${employeeHTML()}
+                </div>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
+    </html>
+    `
+}
+
+function employeeHTML() {
     team.forEach(employee => {
         if (employee.getRole() === "Manager") {
-            console.log('manager');
+            //console.log('manager');
             managerHTML(employee);
         }
         if (employee.getRole() === "Student") {
-            console.log('intern');
+            //console.log('intern');
             internHTML(employee);
         }
         if (employee.getRole() === "Engineer") {
-            console.log('engineer')
+            //console.log('engineer')
             engineerHTML(employee);
         }
     }) 
