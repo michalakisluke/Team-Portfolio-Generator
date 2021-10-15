e293acbfd5a694a66be33be295728235f6a7f916
Luke Michalakis <michalakisluke@gmail.com>
2021-10-15 12:03:10 -0400

    Fix generateHTML function

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function createTeam(response) {
             //console.log(team);
             // I think this should be the function to write the file, right? Then the call would be that function with an argument of the generateHTML() function.
             console.log(generateHTML());
-            //writeFile(generateHTML());
+            writeFile(generateHTML());
     }
     return team;
 }
@@ -136,7 +136,7 @@ function generateHTML() {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="../../src/style.css" rel="stylesheet">
+        <link href="../src/style.css" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <title>Team Portfolio</title>
     </head>
